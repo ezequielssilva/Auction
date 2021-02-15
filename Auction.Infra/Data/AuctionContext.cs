3bc091f523092ea495da31d95e053eb019ea1de4
Ezequiel Silva <ezequiel.silva98@outlook.com>
2021-02-15 09:22:07 -0300

    Configurações iniciais do projeto de infra.

---
+++ Auction.Infra/Data/AuctionContext.cs
@@ -0,0 +1,25 @@
+﻿using Auction.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auction.Infra.Data
+{
+    public class AuctionContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Negotiation> Negotiations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQL;Database=Auction;User Id=sa;Password=E34A18M@gf;");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+            modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<Negotiation>().ToTable("Negotiation");
+        }
+    }
+}
